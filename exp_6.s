e0f78ee6f0e278c767f300587bb1e64c52aa23a2
Tusharprabhu <tusharprabhu2004@gmail.com>
2024-09-13 08:58:04 +0530

    Refactor assembly code for Hamming encoding and add new assembly files

---
+++ exp_6.s
@@ -0,0 +1,20 @@
+.data
+A:.word 18
+divisor:.word 3
+ans:.word 0
+
+.text
+la x11,ans
+lw x10,A
+lw x5,divisor
+loop: blt x10,x0,not_div
+      beq x10,x0,div
+      sub x10,x10,x5
+      j loop
+      
+not_div:addi x10,x0,0
+        j exit
+div: addi x10,x0,1
+     j exit
+exit: sw x10,0(x11) 
+     nop
